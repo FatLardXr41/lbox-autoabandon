0b6204ab573dd97c75577051cba0175d874b77a3
i do shit for fun <148020755+FatLardXr41@users.noreply.github.com>
2024-03-11 11:23:46 +0300

    Add files via upload

---
+++ autoabandon.lua
@@ -0,0 +1,26 @@
+-- autoabandon lua
+-- Made by https://lmaobox.net/forum/v/profile/34545496/somefemboy5141
+--          @
+--      11/03/2024 
+--		  11.18
+
+local function autoabandon()
+    local players = entities.FindByClass("CTFPlayer")
+    local pLocal  = entities.GetLocalPlayer()
+    local friendOnly = false
+    
+    local markedPlayers = {} -- Keep track of marked players
+    
+    for i, player in pairs(players) do -- get players
+		if player == nil then -- if our player is nil, return
+            return;
+        end
+		
+		if #players < 4 then -- if we less than 4 players, disconnect.
+			client.Command("disconnect")
+		end
+	end
+end
+
+callbacks.Unregister("CreateMove", "AutoAbandon")
+callbacks.Register("CreateMove", "AutoAbandon", autoabandon)
